93fab7f623a7d6d0f0c2f7e1c86a1f653be0fcfd
eulinp <eulinpr@gmail.com>
2021-07-06 11:24:20 -0400

    Agregada validez por 1 año a bonoDirecto

--- app/Http/Controllers/ProductWarehouseController.php
+++ app/Http/Controllers/ProductWarehouseController.php
@@ -402,12 +402,16 @@ class ProductWarehouseController extends Controller
         try {
             $padre = User::find(Auth::user()->id)->referred_id;
             $idProducto = $producto;
-            
-            if($idProducto == 1){
-                dd('Se genera el pago de 50$USD al usuario: ' . $padre);
-            }elseif($idProducto == 2){
-                dd('Se genera el pago de 70$USD al usuario: ' . $padre);
+
+            if((User::find(Auth::user()->id)->created_at)->diffInYears(Carbon::now()) <= 1)//Validez por 1 año
+            {
+                if($idProducto == 1){
+                    dd('Se genera el pago de 50$USD al usuario: ' . $padre);
+                }elseif($idProducto == 2){
+                    dd('Se genera el pago de 70$USD al usuario: ' . $padre);
+                }
             }
+           
         } catch (\Throwable $th) {
             dd($th);
         }
