7b602a8c59e655f0229325d7c21297c0f46517d6
Leomiguel19 <leomiguel1907@gmail.com>
2021-07-08 00:38:44 -0400

    Primeros pasos para hacer el cron para los Bonos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use App\Models\BonusPivot;
+use App\Models\Wallet;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +28,25 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            // DB::table('bonus_pivot')->delete();
+            BonusPivot::create([
+                'user_id' => random_int(1,26),
+                'bonuses_id' => random_int(1,7),
+                'status' => random_int(0,2),
+                'bonus_date' => Carbon::now()
+            ]);
+
+            Wallet::create([
+                'user_id' => random_int(1,26),
+                'referred_id' => random_int(1,26),
+                'amount' => random_int(1000,25000),
+                'description' => 'Prueba random',
+                'status' => random_int(0,2),
+                'type_transaction' => random_int(1,2),
+                'liquidated' => '0'            
+            ]);
+        })->everyMinute();        
     }
 
     /**
