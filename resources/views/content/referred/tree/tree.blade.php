1cfef9710d249ebb60736b486dff96601c66599f
eulinp <eulinpr@gmail.com>
2021-07-12 13:37:32 -0400

    Modificación de arbol

--- resources/views/content/referred/tree/tree.blade.php
+++ resources/views/content/referred/tree/tree.blade.php
@@ -9,7 +9,7 @@
 <div class="col-9 text-center">
     <div class="padre">
         <ul>
-            <li class="baseli">
+            <li class="baseli px-0"  style="width:100%;">
                 <a class="base" href="#">
                     <img src="{{$base->logoarbol}}" alt="{{$base->name}}" title="{{$base->name}}" height="96">
                 </a>
@@ -76,17 +76,17 @@
     @endif
 </div>
 <div class="col-3 mt-5">
-    <div class="card">
-        <img src="{{$base->logoarbol}}" width="150px" height="150px" class="mx-auto">
+    <div class="card" id="tarjeta">
+        <img src="{{$base->logoarbol}}" id="#imagen" width="150px" height="150px" class="mx-auto">
         <div class="card-body">
           <h5 class="card-title">Nombre</h5>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">Paquete</li>
-            <li class="list-group-item">Estado</li>
+            {{-- <li class="list-group-item">Paquete</li> --}}
+            <li class="list-group-item" id="estado">Estado</li>
           </ul>
         </div>
         <div class="card-footer">
-            <a href="" class="btn btn-success btn-block disabled">Ver Árbol</a>
+            <a href="" id="ver_arbol" class="btn btn-success btn-block disabled">Ver Árbol</a>
         </div>
       </div>
 </div>
@@ -108,5 +108,32 @@
         document.execCommand("copy");
         textArea.remove();
     }
+
+    function tarjeta(data, url){
+        console.log(url);
+            //console.log('assets/img/sistema/favicon.png');
+            $('.card-title').text("Nombre: " + data.firstname);
+            if(data.profile_photo_url == null){
+                $('#imagen').attr('src', data.logoarbol );   
+            }else{
+                $('#imagen').attr('src', data.profile_photo_url);    
+            }
+            if(url != "" || url != null){
+                $('#ver_arbol').attr('href', url);
+                $('#ver_arbol').removeClass('disabled');
+            }else{
+                $('#ver_arbol').addClass('disabled');
+            }
+            // $('#inversion').text(data.inversion);
+            if(data.status == 0){
+                $('#estado').html('<li>Estado: <span class="badge badge-warning">Inactivo</span></li>');
+            }else if(data.status == 1){
+                $('#estado').html('<li>Estado: <span class="badge badge-success">Activo</span></li>');
+            }else if(data.status == 2){
+                $('#estado').html('<li>Estado: <span class="badge badge-danger">Espanminado</li>');
+            }
+            
+            // $('#tarjeta').removeClass('d-none');
+        }
 
 </script>
