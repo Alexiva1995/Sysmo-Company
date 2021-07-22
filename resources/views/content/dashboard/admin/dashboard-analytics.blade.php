d02ab2c2163e1d38bace6eb618818326383ce097
eulinp <eulinpr@gmail.com>
2021-07-22 12:54:44 -0400

    Cards Dashboard

--- resources/views/content/dashboard/admin/dashboard-analytics.blade.php
+++ resources/views/content/dashboard/admin/dashboard-analytics.blade.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="{{ asset('css/base/plugins/charts/chart-apex.css') }}">
   <link rel="stylesheet" href="{{ asset('css/base/plugins/extensions/ext-component-toastr.css') }}">
   <link rel="stylesheet" href="{{ asset('css/base/pages/app-invoice-list.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/custom-dashboard.css') }}">
   @endsection
 
 @section('content')
@@ -23,219 +24,71 @@
   <div class="row match-height">
     <!-- Greetings Card starts -->
     <div class="col-lg-6 col-md-12 col-sm-12">
-      <div class="card card-congratulations">
-        <div class="card-body text-center">
-          <img
-            src="{{asset('images/elements/decore-left.png')}}"
-            class="congratulations-img-left"
-            alt="card-img-left"
-          />
-          <img
-            src="{{asset('images/elements/decore-right.png')}}"
-            class="congratulations-img-right"
-            alt="card-img-right"
-          />
-          <div class="avatar avatar-xl bg-primary shadow">
-            <div class="avatar-content">
-              <i data-feather="award" class="font-large-1"></i>
+      <div class="card card-light">
+        <div class="card-body text-left">
+          <div class="row">
+            <div class="col-6 d-flex flex-column">
+
+              <h1 class="mb-1 text-dark texto-card-1">Saldo Disponible</h1>
+              <h1 class="mb-1 text-primary texto-card-2">$78.20</h1>
+              
+              <div class="text-left">
+                <p class="card-text m-auto w-75">
+                  <button class="btn  btn-outline-danger border-radius-30 text-dark font-bold px-3">Retirar</button>
+                </p>
+              </div>
+            </div>
+
+            <div class="col-6">
+
+              <div class="img-card">
+                <svg width="151" height="136" viewBox="0 0 151 136" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M135.9 0H22.65C10.1623 0 0 10.1698 0 22.6667V113.333C0 125.83 10.1623 136 22.65 136H135.9C144.228 136 151 129.223 151 120.889V105.778H90.6C82.2723 105.778 75.5 99.0004 75.5 90.6667V45.3333C75.5 36.9996 82.2723 30.2222 90.6 30.2222H151V15.1111C151 6.77733 144.228 0 135.9 0Z" fill="#0050AF" fill-opacity="0.1"/>
+                </svg>
+              </div>
             </div>
           </div>
-          <div class="text-center">
-            <h1 class="mb-1 text-white">Bienvenido Admin: {{ Auth::user()->username }}</h1>
-            <p class="card-text m-auto w-75">
-              <button class="btn btn-danger" data-link="{{Request::url()}}/register?referred_id={{Auth::user()->id}}" id="referrals_link" onclick="copyReferralsLink();">Copiar link de referido <i class="far fa-copy"></i></button>
-            </p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-lg-6 col-md-12 col-sm-12">
+      <div class="card card-congratulations">
+        <div class="card-body text-left">
+          <div class="row">
+            <div class="col-6 d-flex flex-column">
+
+              <h1 class="mb-1 text-white texto-card-1">Membresía</h1>
+              <h1 class="mb-1 text-white texto-card-2">Crypto <br>
+                <span style="font-weight: 300; font-size: 13pt;">Academia</span>
+              </h1>
+              
+              
+            </div>
+
+            <div class="col-6">
+
+              <div class="img-card">
+                <svg width="123" height="85" viewBox="0 0 123 85" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M119.396 0.413483H98.7385V7.62081C98.7385 13.5818 93.8891 18.4314 87.9279 18.4314H35.0721C29.1109 18.4314 24.2615 13.5818 24.2615 7.62081V0.413483H3.60354C1.61319 0.413483 0 2.02691 0 4.01703V80.895C0 82.8852 1.61319 84.4986 3.60354 84.4986H119.396C121.387 84.4986 123 82.8852 123 80.895V4.01703C123 2.02691 121.387 0.413483 119.396 0.413483ZM79.5196 32.2457H94.1692C96.1594 32.2457 97.7728 33.8592 97.7728 35.8493C97.7728 37.8394 96.1594 39.4528 94.1692 39.4528H79.5196C77.5295 39.4528 75.9161 37.8394 75.9161 35.8493C75.9161 33.8592 77.5295 32.2457 79.5196 32.2457ZM50.5697 70.0837H21.7414C19.751 70.0837 18.1378 68.4703 18.1378 66.4801C18.1378 60.0677 21.5076 54.4306 26.567 51.2369C24.6838 49.0327 23.5432 46.1763 23.5432 43.0571C23.5432 36.1025 29.2012 30.4447 36.1556 30.4447C43.1099 30.4447 48.768 36.1025 48.768 43.0571C48.768 46.1763 47.6273 49.0327 45.7441 51.2369C50.8035 54.4306 54.1733 60.0677 54.1733 66.4801C54.1733 68.4703 52.5598 70.0837 50.5697 70.0837ZM101.377 68.2826H72.3118C70.3217 68.2826 68.7083 66.6692 68.7083 64.6791C68.7083 62.689 70.3217 61.0755 72.3118 61.0755H101.377C103.367 61.0755 104.981 62.689 104.981 64.6791C104.981 66.6692 103.367 68.2826 101.377 68.2826ZM101.377 53.8677H72.3118C70.3217 53.8677 68.7083 52.2543 68.7083 50.2642C68.7083 48.2741 70.3217 46.6606 72.3118 46.6606H101.377C103.367 46.6606 104.981 48.2741 104.981 50.2642C104.981 52.2543 103.367 53.8677 101.377 53.8677Z" fill="white" fill-opacity="0.08"/>
+                  </svg>
+              </div>
+            </div>
+            <div class="text-center col-12">
+              <p class="card-text m-auto w-100">
+                {{-- <button class="btn  btn-outline-danger border-radius-30 text-dark font-bold px-3">Retirar</button> --}}
+                <button class="btn btn-danger btn-block" data-link="{{Request::url()}}/register?referred_id={{Auth::user()->id}}" id="referrals_link" onclick="copyReferralsLink();">Copiar link de referido <i class="far fa-copy"></i></button>
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
     <!-- Greetings Card ends -->
 
-    <!-- Subscribers Chart Card starts -->
-    <div class="col-lg-3 col-sm-6 col-12">
-      <div class="card">
-        <div class="card-header flex-column align-items-start pb-0">
-          <div class="avatar bg-light-primary p-50 m-0">
-            <div class="avatar-content">
-              <i data-feather="users" class="font-medium-5"></i>
-            </div>
-          </div>
-          <h2 class="font-weight-bolder mt-1">92.6k</h2>
-          <p class="card-text">Subscribers Gained</p>
-        </div>
-        <div id="gained-chart"></div>
-      </div>
-    </div>
-    <!-- Subscribers Chart Card ends -->
 
-    <!-- Orders Chart Card starts -->
-    <div class="col-lg-3 col-sm-6 col-12">
-      <div class="card">
-        <div class="card-header flex-column align-items-start pb-0">
-          <div class="avatar bg-light-warning p-50 m-0">
-            <div class="avatar-content">
-              <i data-feather="package" class="font-medium-5"></i>
-            </div>
-          </div>
-          <h2 class="font-weight-bolder mt-1">38.4K</h2>
-          <p class="card-text">Orders Received</p>
-        </div>
-        <div id="order-chart"></div>
-      </div>
-    </div>
-    <!-- Orders Chart Card ends -->
   </div>
 
-  <div class="row match-height">
-    <!-- Avg Sessions Chart Card starts -->
-    <div class="col-lg-6 col-12">
-      <div class="card">
-        <div class="card-body">
-          <div class="row pb-50">
-            <div class="col-sm-6 col-12 d-flex justify-content-between flex-column order-sm-1 order-2 mt-1 mt-sm-0">
-              <div class="mb-1 mb-sm-0">
-                <h2 class="font-weight-bolder mb-25">2.7K</h2>
-                <p class="card-text font-weight-bold mb-2">Avg Sessions</p>
-                <div class="font-medium-2">
-                  <span class="text-success mr-25">+5.2%</span>
-                  <span>vs last 7 days</span>
-                </div>
-              </div>
-              <button type="button" class="btn btn-primary">View Details</button>
-            </div>
-            <div class="col-sm-6 col-12 d-flex justify-content-between flex-column text-right order-sm-2 order-1">
-              <div class="dropdown chart-dropdown">
-                <button
-                  class="btn btn-sm border-0 dropdown-toggle p-50"
-                  type="button"
-                  id="dropdownItem5"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Last 7 Days
-                </button>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownItem5">
-                  <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
-                  <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
-                  <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
-                </div>
-              </div>
-              <div id="avg-sessions-chart"></div>
-            </div>
-          </div>
-          <hr />
-          <div class="row avg-sessions pt-50">
-            <div class="col-6 mb-2">
-              <p class="mb-50">Goal: $100000</p>
-              <div class="progress progress-bar-primary" style="height: 6px">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  aria-valuenow="50"
-                  aria-valuemin="50"
-                  aria-valuemax="100"
-                  style="width: 50%"
-                ></div>
-              </div>
-            </div>
-            <div class="col-6 mb-2">
-              <p class="mb-50">Users: 100K</p>
-              <div class="progress progress-bar-warning" style="height: 6px">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  aria-valuenow="60"
-                  aria-valuemin="60"
-                  aria-valuemax="100"
-                  style="width: 60%"
-                ></div>
-              </div>
-            </div>
-            <div class="col-6">
-              <p class="mb-50">Retention: 90%</p>
-              <div class="progress progress-bar-danger" style="height: 6px">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  aria-valuenow="70"
-                  aria-valuemin="70"
-                  aria-valuemax="100"
-                  style="width: 70%"
-                ></div>
-              </div>
-            </div>
-            <div class="col-6">
-              <p class="mb-50">Duration: 1yr</p>
-              <div class="progress progress-bar-success" style="height: 6px">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  aria-valuenow="90"
-                  aria-valuemin="90"
-                  aria-valuemax="100"
-                  style="width: 90%"
-                ></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Avg Sessions Chart Card ends -->
-
-    <!-- Support Tracker Chart Card starts -->
-    <div class="col-lg-6 col-12">
-      <div class="card">
-        <div class="card-header d-flex justify-content-between pb-0">
-          <h4 class="card-title">Support Tracker</h4>
-          <div class="dropdown chart-dropdown">
-            <button
-              class="btn btn-sm border-0 dropdown-toggle p-50"
-              type="button"
-              id="dropdownItem4"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Last 7 Days
-            </button>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownItem4">
-              <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
-              <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
-              <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
-            </div>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-sm-2 col-12 d-flex flex-column flex-wrap text-center">
-              <h1 class="font-large-2 font-weight-bolder mt-2 mb-0">163</h1>
-              <p class="card-text">Tickets</p>
-            </div>
-            <div class="col-sm-10 col-12 d-flex justify-content-center">
-              <div id="support-trackers-chart"></div>
-            </div>
-          </div>
-          <div class="d-flex justify-content-between mt-1">
-            <div class="text-center">
-              <p class="card-text mb-50">New Tickets</p>
-              <span class="font-large-1 font-weight-bold">29</span>
-            </div>
-            <div class="text-center">
-              <p class="card-text mb-50">Open Tickets</p>
-              <span class="font-large-1 font-weight-bold">63</span>
-            </div>
-            <div class="text-center">
-              <p class="card-text mb-50">Response Time</p>
-              <span class="font-large-1 font-weight-bold">1d</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Support Tracker Chart Card ends -->
-  </div>
 
   <div class="row match-height">
     <!-- Timeline Card -->
@@ -467,31 +320,6 @@
     <!--/ App Design Card -->
   </div>
 
-  <!-- List DataTable -->
-  <div class="row">
-    <div class="col-12">
-      <div class="card invoice-list-wrapper">
-        <div class="card-datatable table-responsive">
-          <table class="invoice-list-table table">
-            <thead>
-              <tr>
-                <th></th>
-                <th>#</th>
-                <th><i data-feather="trending-up"></i></th>
-                <th>Client</th>
-                <th>Total</th>
-                <th class="text-truncate">Issued Date</th>
-                <th>Balance</th>
-                <th>Invoice Status</th>
-                <th class="cell-fit">Actions</th>
-              </tr>
-            </thead>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--/ List DataTable -->
 </section>
 <!-- Dashboard Analytics end -->
 @endsection
