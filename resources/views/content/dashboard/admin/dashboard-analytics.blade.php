2477b5c0999ec6cf04e1a98e0d1c182f06175c47
unknown <eulinpr@gmail.com>
2021-07-28 20:10:52 -0400

    Dessabilita el boton retirar cuando el saldo pendiente es 0

--- resources/views/content/dashboard/admin/dashboard-analytics.blade.php
+++ resources/views/content/dashboard/admin/dashboard-analytics.blade.php
@@ -35,7 +35,7 @@
                             <div class="text-left">
                                 <p class="card-text m-auto w-75">
                                     <button
-                                        class="btn  btn-outline-warning border-radius-30 text-dark font-bold px-3">Retirar</button>
+                                        class="btn  btn-outline-warning border-radius-30 text-dark font-bold px-3 @if($total < 1) disabled @endif">Retirar</button>
                                 </p>
                             </div>
                         </div>
