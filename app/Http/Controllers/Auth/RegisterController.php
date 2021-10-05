f8605fd2b51b62bab7fb3ec92c804aa7537f0750
Zelaznog-2 <60021925+Zelaznog-2@users.noreply.github.com>
2021-10-05 13:45:55 -0400

    agregar api para recibir los usuarios de la academia

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Rules\Password;
 use Illuminate\Support\Facades\Validator;
+
 
 class RegisterController extends Controller
 {
@@ -79,5 +83,57 @@ class RegisterController extends Controller
         return view('/auth/register', [
         'pageConfigs' => $pageConfigs
         ]);
+    }
+
+    /**
+     * Permite registrar a los usuarios que vienen de la academia
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function getRegisteApiCrypto(Request $request)
+    {
+            $messages = [
+                'firstname.required' => 'El Campo nombre es requerido',
+                'lastname.required' => 'El Campo apellido es requerido',
+                'username.required' => 'El Campo Nombre de Usuario es requerido.',
+                'username.unique' => 'Ya existe este nombre de usuario en nuestra base de datos',
+                'email.require' => 'El campo Correo es requerido',
+                'email.unique' => 'Ya existe este correo en nuestra base de datos',
+                'token.required' => 'El campo token es requerido',
+            ];
+            $input = $request->toArray();
+            $validate = Validator::make($input, [
+                'token' => ['required'],
+                'firstname' => ['required', 'string', 'max:255'],
+                'lastname' => ['required', 'string', 'max:255'],
+                'username' => ['required', 'string', 'max:255', 'unique:users,username'],
+                'email' => ['required', 'email', 'string', 'max:255', 'unique:users,email'],
+                'password' => ['required', 'string', new Password],
+                'user_id_crypto' => ['required']
+            ], $messages);
+            if ($validate->fails()) {
+                return response()->json(['error'=>$validate->errors()], 401);
+            }
+
+            $token = ($input['token'] == '$2a$15$VRAzVMvVQkqgKMlfIzRPv.4HR3lG.MdpoD60f9Thgw1KaA4WH7Ssy') ? true : false;
+    
+            if ($token) {
+                $user = User::create([
+                    'firstname' => $input['firstname'],
+                    'lastname' => $input['lastname'],
+                    'username' => $input['username'].Carbon::now()->format('Y-m-d-H-i-s'),
+                    'email' => $input['email'],
+                    'password' => Hash::make($input['password']),
+                    'referred_id' => 1,
+                    'user_id_crypto'=> $input['user_id_crypto']
+                ]);
+    
+                if ($user) {
+                    return response()->json(['success' => 'ok']);
+                }
+            }else{
+                return response()->json(['error' => ['token' => 'El token es incorrecto']]);
+            }
     }
 }
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'coinpayment/ipn'
+        'coinpayment/ipn',
+        'v1/register'
     ];
 }
